34129092ec8a515923c049686e94f4d18b1d9272
smokentar <dimitar.atanasov@vulshark.com>
2024-04-10 18:52:56 +0100

    fix(ec2.delete_instances): Add 'disable_protections' parameter allowing disabling of termination and stop API protections before deletion

--- avtomat_aws/cli/ec2/delete_instances.py
+++ avtomat_aws/cli/ec2/delete_instances.py
@@ -1,7 +1,9 @@
 from avtomat_aws.helpers.cli.set_output import set_output
 from avtomat_aws.services.ec2 import delete_instances
 
-ACTION_DESCRIPTION = "Delete EC2 instances."
+ACTION_DESCRIPTION = (
+    "Delete EC2 instances and, optionally, any associated resource types."
+)
 
 
 def add_cli_arguments(parser):
@@ -25,6 +27,12 @@ def add_cli_arguments(parser):
         "--final_image",
         action="store_true",
         help="Create an image (AMI) before deletion.",
+        required=False,
+    )
+    parser.add_argument(
+        "--disable_protections",
+        action="store_true",
+        help="Disable termination and stop protection before deletion.",
         required=False,
     )
 
